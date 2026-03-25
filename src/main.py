996419530a0791909c1a195a3528f946588da66c
Dinud Fernando <85138545+Dinud1231@users.noreply.github.com>
2026-03-25 20:08:00 +0000

    Mar 25, 2026, 4:08 PM

--- src/main.py
+++ src/main.py
@@ -7,3 +7,4 @@ GAME_PATH = os.path.dirname(os.path.abspath(__file__))
 def get_asset_path(filename: str) -> str:
     '''Returns the path to an asset file, given its filename.'''
     return os.path.join(GAME_PATH, "assets", filename)
+
